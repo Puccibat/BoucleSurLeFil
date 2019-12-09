82a2071397c6bd4afb7b53121f80b41f3cc336b5
Baptiste <baptiste.puccinelli@gmail.com>
2019-12-09 15:52:50 +0100

    Category by Id

--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { create } = require('../controller/category');
+const { create, categoryById, read } = require('../controller/category');
 const { userSignupValidator } = require('../validator/index');
 const { requireSignin, isAuth, isAdmin } = require('../controller/auth');
 const { userById } = require('../controller/user');
 
+router.get('/category/:categoryId', read);
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
 
+router.param('categoryId', categoryById);
 router.param('userId', userById);
 
 module.exports = router;

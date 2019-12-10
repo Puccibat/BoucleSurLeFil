bf40598118bcbf6d893990a37646a77d07fa3cbd
Baptiste <baptiste.puccinelli@gmail.com>
2019-12-10 14:18:47 +0100

    Update, remove & list categories

--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const { create, categoryById, read } = require('../controller/category');
+const {
+  create,
+  categoryById,
+  read,
+  update,
+  remove,
+  list
+} = require('../controller/category');
 const { userSignupValidator } = require('../validator/index');
 const { requireSignin, isAuth, isAdmin } = require('../controller/auth');
 const { userById } = require('../controller/user');
 
 router.get('/category/:categoryId', read);
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
+router.put(
+  '/category/:categoryId/:userId',
+  requireSignin,
+  isAuth,
+  isAdmin,
+  update
+);
+router.delete(
+  '/category/:categoryId/:userId',
+  requireSignin,
+  isAuth,
+  isAdmin,
+  remove
+);
+router.get('/categories', list);
 
 router.param('categoryId', categoryById);
 router.param('userId', userById);

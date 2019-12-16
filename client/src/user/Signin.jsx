0ab3ae84f8e0e7b641a7e847e31b1a2f0376bb25
Baptiste <baptiste.puccinelli@gmail.com>
2019-12-16 12:36:04 +0100

    Signin & signup

--- client/src/user/Signin.jsx
+++ client/src/user/Signin.jsx
@@ -1,10 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import Layout from '../core/Layout';
+import { signin } from '../auth/index';
 
-const Signin = () => (
-  <Layout title='Signin Page' description='BoucleSurLeFil'>
-    ...
-  </Layout>
-);
+const Signin = () => {
+  const [values, setvalues] = useState({
+    email: '',
+    password: '',
+    error: '',
+    loading: false,
+    redirectToReferrer: false
+  });
+
+  const { email, password, loading, error, redirectToReferrer } = values;
+
+  const handleChange = name => event => {
+    setvalues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const clickSubmit = event => {
+    event.preventDefault();
+    setvalues({ ...values, error: false, loading: true });
+    signin({ email, password }).then(data => {
+      if (data.error) {
+        setvalues({ ...values, error: data.error, loading: false });
+      } else {
+        setvalues({
+          ...values,
+          redirectToReferrer: true
+        });
+      }
+    });
+  };
+
+  const signinForm = () => (
+    <form>
+      <div className='form-group'>
+        <label className='text-muted'>Email</label>
+        <input
+          type='email'
+          onChange={handleChange('email')}
+          className='form-control'
+          value={email}
+        />
+      </div>
+      <div className='form-group'>
+        <label className='text-muted'>Password</label>
+        <input
+          type='password'
+          onChange={handleChange('password')}
+          className='form-control'
+          value={password}
+        />
+      </div>
+      <button onClick={clickSubmit} className='btn btn-primary'>
+        Submit
+      </button>
+    </form>
+  );
+
+  const showError = () => (
+    <div
+      className='alert alert-danger'
+      style={{ display: error ? '' : 'none' }}
+    >
+      {error}
+    </div>
+  );
+
+  const showLoading = () =>
+    loading && (
+      <div className='alert alert-info'>
+        <h2>Loading...</h2>
+      </div>
+    );
+
+  const redirectUser = () => {
+    if (redirectToReferrer) {
+      return <Redirect to='/' />;
+    }
+  };
+
+  return (
+    <Layout
+      title='Signin'
+      description='BoucleSurLeFil'
+      className='container col-md-8 offset-md-2'
+    >
+      {showLoading()}
+      {showError()}
+      {signinForm()}
+      {redirectUser()}
+    </Layout>
+  );
+};
 
 export default Signin;
--- client/src/user/Signup.jsx
+++ client/src/user/Signup.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Layout from '../core/Layout';
+import { signup } from '../auth/index';
 
 const Signup = () => {
   const [values, setvalues] = useState({
@@ -10,8 +12,29 @@ const Signup = () => {
     succes: false
   });
 
+  const { name, email, password, success, error } = values;
+
   const handleChange = name => event => {
     setvalues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const clickSubmit = event => {
+    event.preventDefault();
+    setvalues({ ...values, error: false });
+    signup({ name, email, password }).then(data => {
+      if (data.error) {
+        setvalues({ ...values, error: data.error, success: false });
+      } else {
+        setvalues({
+          ...values,
+          name: '',
+          email: '',
+          password: '',
+          error: '',
+          success: true
+        });
+      }
+    });
   };
 
   const signupForm = () => (
@@ -22,6 +45,7 @@ const Signup = () => {
           type='text'
           onChange={handleChange('name')}
           className='form-control'
+          value={name}
         />
       </div>
       <div className='form-group'>
@@ -30,6 +54,7 @@ const Signup = () => {
           type='email'
           onChange={handleChange('email')}
           className='form-control'
+          value={email}
         />
       </div>
       <div className='form-group'>
@@ -38,10 +63,31 @@ const Signup = () => {
           type='password'
           onChange={handleChange('password')}
           className='form-control'
+          value={password}
         />
       </div>
-      <button className='btn btn-primary'>Submit</button>
+      <button onClick={clickSubmit} className='btn btn-primary'>
+        Submit
+      </button>
     </form>
+  );
+
+  const showError = () => (
+    <div
+      className='alert alert-danger'
+      style={{ display: error ? '' : 'none' }}
+    >
+      {error}
+    </div>
+  );
+
+  const showSuccess = () => (
+    <div
+      className='alert alert-info'
+      style={{ display: success ? '' : 'none' }}
+    >
+      New account is created. Please <Link to='/signin'>Signin</Link>
+    </div>
   );
 
   return (
@@ -50,8 +96,9 @@ const Signup = () => {
       description='BoucleSurLeFil'
       className='container col-md-8 offset-md-2'
     >
+      {showSuccess()}
+      {showError()}
       {signupForm()}
-      {JSON.stringify(values)}
     </Layout>
   );
 };

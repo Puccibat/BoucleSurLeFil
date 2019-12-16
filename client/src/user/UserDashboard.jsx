d69549f89b15e6ba5e1f8abd39dbb718a9804b0e
Baptiste <baptiste.puccinelli@gmail.com>
2019-12-16 16:51:14 +0100

    Links in user dashboard

--- client/src/user/UserDashboard.jsx
+++ client/src/user/UserDashboard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Layout from '../core/Layout';
 import { isAuth } from '../auth/index';
 
@@ -7,12 +8,28 @@ const UserDashboard = () => {
     user: { _id, name, email, role }
   } = isAuth();
 
-  return (
-    <Layout
-      title='Dashboard'
-      description='User dashboard'
-      className='container'
-    >
+  const userLinks = () => {
+    return (
+      <div className='card'>
+        <h4 className='card-header'></h4>
+        <ul className='list-group'>
+          <li className='list-group-item'>
+            <Link className='nav-link' to='/cart'>
+              My cart
+            </Link>
+          </li>
+          <li className='list-group-item'>
+            <Link className='nav-link' to='/profile/update'>
+              Update profile
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  const userInfo = () => {
+    return (
       <div className='card mb-5'>
         <h3 className='card-header'>User information</h3>
         <ul className='list-group'>
@@ -23,12 +40,32 @@ const UserDashboard = () => {
           </li>
         </ul>
       </div>
+    );
+  };
 
+  const purchaseHistory = () => {
+    return (
       <div className='card mb-5'>
         <h3 className='card-header'>Purchase history</h3>
         <ul className='list-group'>
           <li className='list-group-item'>history</li>
         </ul>
+      </div>
+    );
+  };
+
+  return (
+    <Layout
+      title='Dashboard'
+      description={`Hey ${name} !`}
+      className='container'
+    >
+      <div className='row'>
+        <div className='col-3'>{userLinks()}</div>
+        <div className='col-9'>
+          {userInfo()}
+          {purchaseHistory()}
+        </div>
       </div>
     </Layout>
   );

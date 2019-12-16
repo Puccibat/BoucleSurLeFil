e7d715c6badef4bbd352ab5380fbcdde0f80526d
Baptiste <baptiste.puccinelli@gmail.com>
2019-12-16 15:42:10 +0100

    Private route for auth user

--- client/src/core/Menu.jsx
+++ client/src/core/Menu.jsx
@@ -20,6 +20,16 @@ const Menu = ({ history }) => {
           </Link>
         </li>
 
+        <li className='nav-item'>
+          <Link
+            className='nav-link'
+            style={isActive(history, '/dashboard')}
+            to='/dashboard'
+          >
+            Dashboard
+          </Link>
+        </li>
+
         {!isAuth() && (
           <Fragment>
             <li className='nav-item'>

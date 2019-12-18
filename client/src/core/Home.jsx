6578a1e708016dcaeefe7b9751cf4d0b6144eae1
Baptiste <baptiste.puccinelli@gmail.com>
2019-12-18 15:08:40 +0100

    Products in home page

--- client/src/core/Home.jsx
+++ client/src/core/Home.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
+import { getProducts } from './apiCore';
 
-const Home = () => (
-  <Layout title='Home Page' description='BoucleSurLeFil'>
-    ...
-  </Layout>
-);
+const Home = () => {
+  const [productsBySell, setPorductsBySell] = useState([]);
+  const [productsByArrivel, setPorductsByArrival] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadProductsBySell = () => {
+    getProducts('sold').then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setPorductsBySell(data);
+      }
+    });
+  };
+
+  const loadProductsByArrival = () => {
+    getProducts('createdAt').then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setPorductsByArrival(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProductsByArrival();
+    loadProductsBySell();
+  }, []);
+
+  return (
+    <Layout title='Home Page' description='BoucleSurLeFil'>
+      {JSON.stringify(productsByArrivel)}
+      <br />
+      {JSON.stringify(productsBySell)}
+    </Layout>
+  );
+};
 
 export default Home;

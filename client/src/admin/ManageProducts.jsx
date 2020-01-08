d9abd8ea41828fe723e1ce4a945e50274ce72fbc
Baptiste <baptiste.puccinelli@gmail.com>
2020-01-08 10:24:10 +0100

    Delete product from dashboard

---
+++ client/src/admin/ManageProducts.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Layout from '../core/Layout';
+import { isAuth } from '../auth/index';
+import { createCategory } from '../admin/ApiAdmin';
+import { getProducts, deleteProduct } from './ApiAdmin';
+
+const ManageProducts = () => {
+  const [products, setProducts] = useState([]);
+
+  const { user, token } = isAuth();
+
+  const loadProducts = () => {
+    getProducts().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setProducts(data);
+      }
+    });
+  };
+
+  const destroy = productId => {
+    deleteProduct(productId, user._id, token).then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        loadProducts();
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+  return (
+    <Layout
+      title='Manage Products'
+      description='Hey ! Please add a new product'
+      className='container'
+    >
+      <div className='row'>
+        <div className='col-12'>
+          <ul className='list-group'>
+            {products.map((product, index) => (
+              <li
+                key={index}
+                className='list-group-item d-flex justify-content-between align-items-center'
+              >
+                <strong>{product.name}</strong>
+                <Link to={`/admin/product/update/${product._id}`}>
+                  <span className='badge badge-warning badge-pill'>Update</span>
+                </Link>
+                <span
+                  onClick={() => destroy(product._id)}
+                  className='badge badge-danger badge-pill'
+                >
+                  Delete
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default ManageProducts;

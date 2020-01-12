d5b70717fd2a459ed0bd6fa20a1532f8f6e4a9cb
Baptiste <baptiste.puccinelli@gmail.com>
2020-01-12 13:15:58 +0100

    Update token expiring date

--- controller/auth.js
+++ controller/auth.js
@@ -35,7 +35,7 @@ exports.signin = (req, res) => {
     // generate a signed token with user id and secret
     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
     // persist the token as 't' in cookie with expiry date
-    res.cookie('t', token, { expire: new Date() + 9999 });
+    res.cookie('t', token, { expire: new Date() + 2000 });
     // return response with user and token to frontend client
     const { _id, name, email, role } = user;
     return res.json({ token, user: { _id, email, name, role } });
